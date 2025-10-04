c850e55004f857f97c44567563a92607d2e39d59
Emperorcray <155745445+Emperorcray@users.noreply.github.com>
2025-10-04 15:05:33 +0100

    commit

---
+++ frontendscripts/script.js
@@ -0,0 +1,13 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+/* for the cart button */
+function cartalert() {
+  document.getElementById("mycart").classList.toggle("show");
+}
